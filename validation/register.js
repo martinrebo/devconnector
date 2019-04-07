3a059196bdadccea866e47440b229f26b44fa904
martinrebo <martinrg33@gmail.com>
2019-04-07 09:50:54 +0200

    Add Profile Route

--- validation/register.js
+++ validation/register.js
@@ -4,12 +4,39 @@ const isEmpty = require('./is-empty');
 
 module.exports = function validateRegisterInput(data) {
     let errors = {};
+
+    data.name = !isEmpty(data.name) ? data.name : '';
+    data.email = !isEmpty(data.email) ? data.email : '';
+    data.password = !isEmpty(data.password) ? data.password : '';
+    data.password2 = !isEmpty(data.password2) ? data.password2 : '';
     
     if(!Validator.isLength(data.name), { min:2, max: 30 }){
 errors.name = 'Name must be between 2 and 30 characters';
     }
 
-    return{
+if( Validator.isEmpty(data.name)) {
+    errors.name = 'Email field is required';
+}
+
+if( Validator.isEmpty(data.email)) {
+    errors.email = 'email field is invalid';
+}
+
+if(!Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is invalid';
+}
+
+if( !Validator.isLength(data.password, {min:6, max:30})) {
+    errors.password = 'Password must be at leas 6 characters';
+}
+if( !Validator.equals(data.password, data.password2)) {
+    errors.password2 = 'Passwords must match';
+}
+
+
+
+
+return{
         errors,
         isValid: isEmpty(errors)
     }
